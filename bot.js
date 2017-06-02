2a6528ef41731b57a7ec427d3187d31b383c4d71
Logan Saso <logan.saso2016@gmail.com>
2017-06-02 13:46:12 -0700

    ignored a few new things

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 var irc = require("irc");
 var redis = require("redis");
-var string_hash = require("string-hash");
+var hash = require("string-hash");
 var config = require("./config.js").config;
+
+var client = redis.createClient();
 
 var bot = new irc.Client(config.server, config.botName, {
 	channels: config.channels,
@@ -9,5 +11,23 @@ var bot = new irc.Client(config.server, config.botName, {
 });
 
 bot.addListener("message", function (from, to, text, message) {
-	console.log(from + ": " + text);
+	console.log("Processing message \"" + text + "\" from " + from);
+	var hash_key = hash(text);
+	console.log("hash: " + hash_key);
+
+	client.get(hash_key, function(err, reply){
+		if(reply == null){
+			client.hset(hash_key, text);
+		} else {
+			client.hincrby(hash_key, text, 1);
+		}
+	});
+
 });
+
+
+client.addListener("error", function(vars){
+	console.log(vars);
+}); //Do nothing on redis error
+
+bot.addListener("error", function(vars){}); //Do nothing on error. It still connects properly but twitch has a weird command.
