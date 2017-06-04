077771c39df6a7f811cf58384e7923a4292597c3
Logan Saso <logan.saso2016@gmail.com>
2017-06-04 01:57:56 -0700

    Redis commands actually work now. Ignored .rdb files.

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+console.log("Script starting");
+
 var irc = require("irc");
 var redis = require("redis");
 var hash = require("string-hash");
@@ -10,16 +12,20 @@ var bot = new irc.Client(config.server, config.botName, {
 	password: config.password
 });
 
+var time_start = new Date();
+var count = 0;
+
+
 bot.addListener("message", function (from, to, text, message) {
 	console.log("Processing message \"" + text + "\" from " + from);
 	var hash_key = hash(text);
-	console.log("hash: " + hash_key);
-
 	client.get(hash_key, function(err, reply){
-		if(reply == null){
-			client.hset(hash_key, text);
+		if(reply === null){
+			client.hset(hash_key, "text", text);
+			client.hset(hash_key, "num", 1);
+			client.hset(hash_key, "author", from);
 		} else {
-			client.hincrby(hash_key, text, 1);
+			client.hincrby(hash_key, "num", 1);
 		}
 	});
 
@@ -30,4 +36,6 @@ client.addListener("error", function(vars){
 	console.log(vars);
 }); //Do nothing on redis error
 
-bot.addListener("error", function(vars){}); //Do nothing on error. It still connects properly but twitch has a weird command.
+bot.addListener("error", function(vars){
+	console.log(vars);
+}); //Do nothing on error. It still connects properly but twitch has a weird command.
