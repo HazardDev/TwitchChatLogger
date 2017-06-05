aed7b10a3864cea3679af7de8558f08b9ae2ddb3
Logan Saso <logan.saso2016@gmail.com>
2017-06-05 13:29:03 -0700

    Found the authors that started it all

--- sort_data.js
+++ sort_data.js
@@ -2,23 +2,28 @@ var parsed_json = require("./pretty_dump.json");
 
 var large_keys = [];
 
-// var num = 0;
+var num = 0;
 
 for (var key in parsed_json) {
-	if (parsed_json[key].value.num > 1) {
-		console.log(parsed_json[key].value.num + ": " + parsed_json[key].value.text);
-		large_keys.push(parsed_json[key])
+	// console.log(parsed_json[key].value.num > 200);
+	if (parsed_json[key].value.num > 200) {
+		// console.log(parsed_json[key].value.num + ": " + parsed_json[key].value.text);
+		large_keys.push(parsed_json[key]);
+		// num += parseInt(parsed_json[key].value.num);
 	}
 
-	// num += parseInt(parsed_json[key].value.num);
+
+	// large_keys.push();
+
 }
 
-// console.log(num / 190010); // average
 
 large_keys.sort(function (a, b) {
 	return a.value.num - b.value.num;
-})
+}).reverse();
 
 for (var object in large_keys) {
-	console.log(large_keys[object].value.num + ": " + large_keys[object].value.text);
+	console.log(large_keys[object].value.num + ": " + large_keys[object].value.author);
 }
+
+// console.log(num / 190010); // average
